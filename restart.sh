ed5b730961d1625bff61c6a117ce28bf6f8034d6
lionng <154704230@qq.com>
2021-12-17 22:55:47 +0800

    update

--- restart.sh
+++ restart.sh
@@ -3,24 +3,25 @@
 # github: https://github.com/Alvin9999/new-pac/wiki/Goflyway%E5%85%8D%E8%B4%B9%E8%B4%A6%E5%8F%B7
 
 
-proxy_ip=144.202.127.132
+proxy_ip=149.28.67.180
 proxy_port=12345
 proxy_password=dongtaiwang.com
 local_port=8100
 goflyway_dir=`pwd`
 
 # kill 8100 goflyway
+# lsof -i:8100 | awk '{print $2}' | grep -v PID | xargs kill 
 pid=`netstat -tnlp | grep ':'${local_port} | awk '{print $7}' | awk -F '/' '{print $1}'`
 if [ $pid ]
 then
-	echo "goflyway exist pid:${pid} !!!  running  kill"
+	echo `date "+%Y-%m-%d %H:%M:%S  "` "goflyway exist pid:${pid} !!!  running  kill"
 	kill ${pid}
 else
-	echo 'goflyway not exist!'
+	echo `date "+%Y-%m-%d %H:%M:%S  "` 'goflyway not exist!' 
 fi
 
 # start goflyway
-echo 'go to start goflyway!'
+echo `date "+%Y-%m-%d %H:%M:%S"` 'go to start goflyway!' 
 nohup ${goflyway_dir}'/goflyway' -up="${proxy_ip}:${proxy_port}" -k="${proxy_password}" -l=":"${local_port} > firefly.log 2>&1 &
 
 
